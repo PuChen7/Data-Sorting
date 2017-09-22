295d767b1e48c416da1bbb7a495017fd2b1c0d82
PuChen7 <puchen8295@gmail.com>
2017-09-21 22:35:10 -0400

    get the number of columns in csv file

--- sorter.c
+++ sorter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv){
     // check for command line input
@@ -9,8 +10,24 @@ int main(int argc, char** argv){
     }
     // get the sort value type
     char* sort_value_type = argv[2];
-    printf("%s", sort_value_type);
 
+    char line[1024];
+    int isFirstRow = 0; // flag for marking the first row
+    int columnNumber = 0;   // holding the number of columns
+    while (fgets(line, 1024, stdin)){
+        char* str = strdup(line);   // read file line by line
+        // get the column number
+        if (isFirstRow == 0){
+            int i = 0;
+            for (; i < strlen(str); i++){
+                if (str[i] == ','){
+                    columnNumber++;
+                }
+            }
+            columnNumber += 1;
+            isFirstRow = 1;
+        }
+    }
     
     return 0;
 }
