41cb32c1ec1d3c55c8b35b21681529e584565ef0
PuChen7 <puchen8295@gmail.com>
2017-09-22 23:29:46 -0400

    successfuly created Linked List and stored data in the list

--- sorter.c
+++ sorter.c
@@ -5,6 +5,14 @@
 #define sorter_header "sorter.h"
 #include sorter_header
 
+struct node{
+    int marker; // hold the index number of the row
+    struct node *next;  // pointer points to the next row
+    char **line_array;  // hold the entire row
+};
+
+/* This array contains the first row of the csv file.
+    It stores all the value types. */
 void initValueTypesArray(char** array,int arraySize,char* line){
     char *token = strtok(line, ",");
     int counter=0;
@@ -25,45 +33,85 @@ int main(int argc, char** argv){
     // get the sort value type
     char* sort_value_type = argv[2];
 
-    char line[1024];
-    int lineCounter=0;
-    int attributesCount;
-    char* tempLine;
+    // head of the Linked List
+    struct node *head = NULL;
+    struct node *prev = NULL;
+
+    char line[1024];    // temp array for holding csv file lines.
+    int rowNumber=0;    // hold the number of lines of the csv file.
+    int value_type_number;    // hold the number of value types(column numbers).
+    char* headerLine;   // hold the first line of the csv file, which is the value types.
+    int isFirstElement = 0; // mark the first element in LL
     while (fgets(line, 1024, stdin)){
-        lineCounter++;
+        rowNumber++;
         char* tmp = strdup(line);        
-        
+        // first row
         // Returns first token 
         char *token = strtok(tmp, ",");
-        if(lineCounter==1){
-            tempLine = strdup(line);
+        if(rowNumber==1){
+            headerLine = strdup(line);
+            value_type_number = 0;
+            while (token != NULL){
+                if(token[strlen(token)-1] == '\n'){
+                    token[strlen(token)-1]=0;//make it end of string         
+                }
+                token = strtok(NULL, ",");
+                value_type_number++;    // update the number of columns(value types).
+            }
+            continue;
         }
-           attributesCount = 0;
+
+        /* malloc array for holding tokens.*/
+        char** new_array = malloc(value_type_number * sizeof(char*));
+
+        // using token to split each line.
+        // store each token into corresponding array cell.
+        int counter = 0;
         while (token != NULL){
             if(token[strlen(token)-1] == '\n'){
                 token[strlen(token)-1]=0;//make it end of string         
             }
-            printf("%s \n", token);
-
             token = strtok(NULL, ",");
-            attributesCount++;
+            new_array[counter] = token; // store token into array
+            counter++;
         }
-        //printf("%d atr\n",attributesCount);
+
+
+        // create a new node
+        struct node *temp = (struct node*) malloc(sizeof(struct node));
+        temp-> marker = rowNumber;
+        temp-> line_array = new_array;
+        temp-> next = NULL;
+
+        //printf("%s\n", temp-> line_array[0]);
+
+        if (isFirstElement == 0){
+            temp-> next = head;
+            head = temp;
+            isFirstElement = 1;
+            prev = head;
+            continue;
+        }
+        prev-> next = temp;
+        prev = temp;
+        
+        //printf("%s \n",prev-> line_array);
 		//printf("Field 1 would be %s\n", getfield(tmp, 1));
 		// NOTE strtok clobbers tmp
-        free(tmp);
-        free(token);
     }
     
     //may need to make sure attrCount are same for all string..
     //or may not....
-    printf("%d lines ",lineCounter);
-    char* headerArray[attributesCount];
-    initValueTypesArray(headerArray,attributesCount,tempLine);
-
-    printf("\n5th header is %s ",headerArray[4]);
-    free(tempLine);    
-    free(headerArray);
+    char* headerArray[value_type_number];
+    initValueTypesArray(headerArray,value_type_number,headerLine);    
     
+    // free Linked List
+    struct node *tmp;
+    while (head != NULL){
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+
     return 0;
 }
