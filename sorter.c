ab4c2e346bec587ab730538ba8ac4427cb13eabd
PuChen7 <puchen8295@gmail.com>
2017-09-25 10:41:13 -0400

    store the column into array

--- sorter.c
+++ sorter.c
@@ -4,14 +4,19 @@
 #include <errno.h>
 #define sorter_header "sorter.h"
 #include sorter_header
+#include <ctype.h>
 
 struct node{
     struct node *next;  // pointer points to the next row
     char **line_array;  // hold the entire row
 };
 
-char *strtok_single (char * str, char const * delims)
-    {
+typedef struct{
+    int index;  // hold the original index number
+    char* str;  // hold the string value
+} SortArray;
+
+char *strtok_single (char * str, char const * delims) {
     static char  * src = NULL;
     char * p,  * ret = 0;
 
@@ -30,13 +35,12 @@ char *strtok_single (char * str, char const * delims)
         ret = src;
         src = NULL;
     }
-
     return ret;
-    }
+}
 
 
-    /* This array contains the first row of the csv file.
-    It stores all the value types. */
+/* This array contains the first row of the csv file.
+It stores all the value types. */
 void initValueTypesArray(char** array,int arraySize,char* line){
     char *token = strtok(line, ",");
     int counter=0;
@@ -54,9 +58,7 @@ void initDataArray(char* array[dataRow][dataCol],struct node * data){
     for(int i = 0;i<dataRow;i++){
         for(int j = 0; j< dataCol ;j++){
             array[i][j]=data->line_array[j];
-            //printf("%s,",array[i][j]);
         }
-        //printf("\n");        
         data = data->next;
     }
 }
@@ -104,7 +106,6 @@ int main(int argc, char** argv){
         /* malloc array for holding tokens.*/
         char** new_array = malloc(value_type_number * sizeof(char*));
 
-
         // using token to split each line.
         // store each token into corresponding array cell.
         int counter = 0;
@@ -113,21 +114,16 @@ int main(int argc, char** argv){
                 token[strlen(token)-1]=0;//make it end of string         
             }
             new_array[counter] = *token ? token : "<empty>"; // store token into array
-            //printf("column %d : %s ",counter,token);
             token = strtok_single(NULL, ",");
             counter++;
         }
 
-
         // create a new node
-        //rowNumber starts from 1
+        // rowNumber starts from 1
         struct node *temp = (struct node*) malloc(sizeof(struct node));
         temp-> line_array = new_array;
         temp-> next = NULL;
 
-        //printf("%s\n", temp-> line_array[0]);
-        //printf("\n row %d \n",rowNumber);
-        
         if (isFirstElement == 0){
             temp-> next = head;
             head = temp;
@@ -137,15 +133,11 @@ int main(int argc, char** argv){
         }
         prev-> next = temp;
         prev = temp;
-        
-        //printf("%s \n",prev-> line_array);
-		//printf("Field 1 would be %s\n", getfield(tmp, 1));
-		// NOTE strtok clobbers tmp
     }
     
     //may need to make sure attrCount are same for all string..
     //or may not....
-    char* headerArray[value_type_number];
+    char* headerArray[value_type_number];   // this array hold the first row.
     initValueTypesArray(headerArray,value_type_number,headerLine);    
     
 
@@ -158,7 +150,6 @@ int main(int argc, char** argv){
     dataRow = rowNumber;
     char* dataArray[dataRow][dataCol];
     initDataArray(dataArray,temp);
-    //printf("%s\n",dataArray[0][0]); currently test.txt this is ruicheng
 
     // free Linked List
     struct node *tmp;
@@ -168,5 +159,31 @@ int main(int argc, char** argv){
         free(tmp);
     }
 
+    /* sorintg */
+    // first need to decide which column to sort. Do the search on headerArray.
+    int i = 0;
+    int isFound = 1;
+    for (; i < value_type_number; i++){
+        if (strcmp(headerArray[i], sort_value_type) == 0){
+            isFound = 0;    // need to check if the csv file has this type.
+            break;  // i is the index of the column
+        }
+    }
+    // the type is not found in the file. ERROR.
+    if (isFound == 1){
+        printf("Error:The value type was not found in csv file!\n");
+        exit(0);
+    }
+
+    // store the column as an array
+    SortArray *sort_array;
+    sort_array = (SortArray*) malloc(rowNumber * sizeof(SortArray));
+    int count = 0;
+    for (; count < rowNumber; count++){
+        sort_array[count].index = count;
+        sort_array[count].str = dataArray[count][i];
+    }
+
+    
     return 0;
 }
