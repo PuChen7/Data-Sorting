d4e86f9b36fb5f9dbebdedb30b955f500247b93b
PuChen7 <puchen8295@gmail.com>
2017-10-01 23:55:59 -0400

    header

--- sorter.c
+++ sorter.c
@@ -7,17 +7,6 @@
 #include <ctype.h>
 #define EMPTY_STRING ""
 
-
-struct node{
-    struct node *next;  // pointer points to the next row
-    char **line_array;  // hold the entire row
-};
-
-typedef struct{
-    int index;  // hold the original index number
-    char* str;  // hold the string value
-} SortArray;
-
 char *strtok_single (char * str, char const * delims) {
     static char  * src = NULL;
     char * p,  * ret = 0;
@@ -40,7 +29,6 @@ char *strtok_single (char * str, char const * delims) {
     return ret;
 }
 
-
 /* This array contains the first row of the csv file.
 It stores all the value types. */
 void initValueTypesArray(char** array,int arraySize,char* line){
@@ -55,7 +43,6 @@ void initValueTypesArray(char** array,int arraySize,char* line){
 }
 
 //global value to for column and row
-int static dataRow,dataCol;
 void initDataArray(char* array[dataRow][dataCol],struct node * data){
     int i,j;
 	i = 0;
@@ -76,90 +63,6 @@ int isNumeric(char* str){
     strtod (str, &p);
     return *p == '\0';
 }
-
-// helper function for sorting numbers
-void merge(SortArray* sort_array, int left, int middle, int right,int numeric){
-    int i, j, k;
-    int n1 = middle - left + 1;
-    int n2 =  right - middle;
-    
-    // temporary arrays
-    SortArray *L;
-    L = (SortArray*) malloc(n1 * sizeof(SortArray));
-    SortArray *R;
-    R = (SortArray*) malloc(n2 * sizeof(SortArray));
-
-    // copy data into temporary arrays
-    for (i = 0; i < n1; i++){
-        L[i].str = sort_array[left + i].str;
-        L[i].index = sort_array[left + i].index;
-    }
-    for (j = 0; j < n2; j++){
-        R[j].str = sort_array[middle + 1+ j].str;
-        R[j].index = sort_array[middle + 1 + j].index;
-    }
-
-    i = 0; 
-    j = 0; 
-    k = left;
-
-    while (i < n1 && j < n2) {
-        if(numeric==0){//if str
-            int cmpResult = strcmp(L[i].str,R[j].str);
-            if (cmpResult<=0) {
-                sort_array[k].str = L[i].str;
-                sort_array[k].index = L[i].index;
-                i++;
-            }
-            else {
-                sort_array[k].str = R[j].str;
-                sort_array[k].index = R[j].index;
-                j++;
-            }
-        }
-        else{//if numeric
-            if (atoi(L[i].str) <= atoi(R[j].str)) {
-                sort_array[k].str = L[i].str;
-                sort_array[k].index = L[i].index;
-                i++;
-            }
-            else {
-                sort_array[k].str = R[j].str;
-                sort_array[k].index = R[j].index;
-                j++;
-            }
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        sort_array[k].str = L[i].str;
-        sort_array[k].index = L[i].index;
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        sort_array[k].str = R[j].str;
-        sort_array[k].index = R[j].index;
-        j++;
-        k++;
-    }
-
-    free(L);
-    free(R);
-
-}
-
-// function for sorting numbers
-void mergeSort(SortArray* sort_array, int left, int right,int numeric){
-    if (left < right){
-        int middle = left + (right - left) / 2;
-        mergeSort(sort_array, left, middle,numeric);
-        mergeSort(sort_array, middle+1, right,numeric);
-        merge(sort_array, left, middle, right,numeric);
-    }
-} 
 
 char *trimwhitespace(char *str) {
   char *end;
@@ -200,8 +103,6 @@ int main(int argc, char** argv){
     }
     int analyzeFlag = 0;
     if(analyzeCmd!= NULL && strcmp(analyzeCmd,"-a")==0)analyzeFlag=1;
-
-    
 
     // head of the Linked List
     struct node *head = NULL;
@@ -449,7 +350,5 @@ int main(int argc, char** argv){
             i==(dataCol-1)?printf("%s\n",dataArray[sort_array[count].index][i]):printf("%s,",dataArray[sort_array[count].index][i]);
         }//end of line
     }
-    
-
     return 0;
 }
