4b37df70b287ad12155d4340451317ec845e12b6
razor <razor.wu1994@gmail.com>
2017-10-26 01:28:49 -0400

    try

--- project1/forktest.c
+++ project1/forktest.c
@@ -10,8 +10,24 @@
 #include <sys/types.h>
 
 static int *pCounter;
+static pid_t *pidArray;
+static int *arrayCursor;
+static char **fileArray;
 pid_t init_pid;
 
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+int indexOfPidArray(pid_t pid){
+    int i=0;
+    for(;i<256;i++){
+      if(pidArray[i]==pid)break;
+    }
+    return i;
+}
 void recur(DIR *pDir, struct dirent *pDirent, char* path){
     pid_t pid,wpid;
     int status = 0;
@@ -23,6 +39,26 @@ void recur(DIR *pDir, struct dirent *pDirent, char* path){
     while ((pDirent = readdir(pDir)) != NULL) {
         if (pDirent->d_name[0] == '.'){continue;}
         //printf("scanning file %s\n", pDirent->d_name);
+
+        if(strchr(pDirent->d_name, '.') != NULL){
+          if(strcmp("csv",get_filename_ext(pDirent->d_name))==0){
+            pid = fork();
+            if (pid < 0){
+                printf("error\n");
+                exit(0);
+            } else if (pid == 0){   // child,aka sorting file
+                pidArray[*arrayCursor]=getpid();
+                fileArray[*arrayCursor]=pDirent->d_name;
+                *arrayCursor=*arrayCursor+1;
+            } else if (pid > 0){    // parent
+                //printf("found directory %s\n", pDirent->d_name);
+            }
+          }else{
+            //other files or invalid csv
+          }
+
+          
+        }
         if (strchr(pDirent->d_name, '.') == NULL){//search for folder
             pid = fork();
             if (pid < 0){
@@ -30,6 +66,7 @@ void recur(DIR *pDir, struct dirent *pDirent, char* path){
                 exit(0);
             } else if (pid == 0){   // child,aka entered directory
                 //printf("enter directory %s\n", pDirent->d_name);
+
                 strcat(path, "/");
                 DIR *newdir = opendir(strcat(path,pDirent->d_name));
                 recur(newdir, pDirent, path);
@@ -55,8 +92,15 @@ void recur(DIR *pDir, struct dirent *pDirent, char* path){
 
     if(getpid()==init_pid){
       wait(NULL);
-      printf("\nTotal Child Processes Number: %d ",*pCounter);
+      printf("\nTotal Child Processes Number: %d \n",*pCounter);
+
+      int counter=0;
+      for(;counter<2;counter++)
+        printf("%d\n",pidArray[counter] );
       munmap(pCounter, sizeof *pCounter);
+      munmap(pidArray, sizeof *pidArray);
+      munmap(arrayCursor, sizeof *arrayCursor);
+
     }
 
 
@@ -69,6 +113,16 @@ int main(int c, char *v[]){
     pCounter = mmap(NULL, sizeof *pCounter, PROT_READ | PROT_WRITE,
                         MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *pCounter = 0;
+
+    pidArray = mmap(NULL, sizeof *pidArray * 256, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    fileArray = mmap(NULL, sizeof *fileArray * 256, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    arrayCursor= mmap(NULL, sizeof *arrayCursor, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *arrayCursor=0;
 
     if(c==1){
       if (getcwd(cwd, sizeof(cwd)) != NULL){
@@ -88,6 +142,7 @@ int main(int c, char *v[]){
     printf("Initial PID : %d\n", (init_pid=getpid()));
 
     recur(pDir, pDirent, ptr);
+
 
     return 0;
 }
