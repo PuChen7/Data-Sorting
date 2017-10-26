00f8d1e8540dbb3a653e824cd85d2e9d56bee1ac
razor <razor.wu1994@gmail.com>
2017-10-26 00:23:55 -0400

    STDOUT good

--- project1/forkDemo.c
+++ project1/forkDemo.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
- 
 
+static int x =1;
 int main() {
-    extern static int x = 1; 
     // make two process which run same
     // program after this instruction
-    if(fork()==0){//child
+    pid_t init_pid;
+    pid_t pid = fork();
+    init_pid= pid;
+    for(int i=1;i<2;i++){
+      pid = fork();
+    }
+    if(pid==0){//child
         x++;
-        printf("%d\n",x);
+        //printf("child x%d\n",x);
     }
-    else{//parent   
+    else{//parent
         x--;
-        printf("%d\n",x);
+        //printf("parent %d\n",x);
     }
- 
-    printf("%d\n",x);
+    printf("init pid %d\n",init_pid);
+    printf("main %d\n",pid);
     return 0;
 }
--- project1/forktest.c
+++ project1/forktest.c
@@ -5,9 +5,16 @@
 #include <string.h>
 #include <ftw.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+static int *pCounter;
+pid_t init_pid;
 
 void recur(DIR *pDir, struct dirent *pDirent, char* path){
-    pid_t pid;
+    pid_t pid,wpid;
+    int status = 0;
     /*
     if (pDir == NULL) {
         printf ("Cannot open directory '%s'\n", v[1]);
@@ -15,35 +22,71 @@ void recur(DIR *pDir, struct dirent *pDirent, char* path){
     }*/
     while ((pDirent = readdir(pDir)) != NULL) {
         if (pDirent->d_name[0] == '.'){continue;}
-        printf("This is %s\n", pDirent->d_name);
-        if (strchr(pDirent->d_name, '.') == NULL){
+        //printf("scanning file %s\n", pDirent->d_name);
+        if (strchr(pDirent->d_name, '.') == NULL){//search for folder
             pid = fork();
             if (pid < 0){
                 printf("error\n");
                 exit(0);
-            } else if (pid == 0){   // child
-                printf("child in %s\n", pDirent->d_name);
+            } else if (pid == 0){   // child,aka entered directory
+                //printf("enter directory %s\n", pDirent->d_name);
                 strcat(path, "/");
                 DIR *newdir = opendir(strcat(path,pDirent->d_name));
                 recur(newdir, pDirent, path);
                 /*if ((pDirent = readdir(newdir)) != NULL){
                     printf("KKKK");
                 }*/
-            } else if (pid > 0){    // parent
-                printf("parent in %s\n", pDirent->d_name);
+            } else if (pid > 0){    // parent ,aka found the directory
+                //printf("found directory %s\n", pDirent->d_name);
             }
         }
     }
     closedir (pDir);
+
+
+    while ((wpid = wait(&status)) > 0)
+    {
+        if(*pCounter==0)printf("Child Processes: ");
+        printf("%d,",wpid);
+        *pCounter+=1;
+        // printf("Exit status of %d was %d (%s)\n", (int)wpid, status,
+        //        (status > 0) ? "accept" : "reject");
+    }
+
+    if(getpid()==init_pid){
+      wait(NULL);
+      printf("\nTotal Child Processes Number: %d ",*pCounter);
+      munmap(pCounter, sizeof *pCounter);
+    }
+
+
 }
 
 int main(int c, char *v[]){
     struct dirent *pDirent;
     DIR *pDir;
+    char cwd[1024];
+    pCounter = mmap(NULL, sizeof *pCounter, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *pCounter = 0;
+
+    if(c==1){
+      if (getcwd(cwd, sizeof(cwd)) != NULL){
+      //fprintf(stdout, "Current working dir: %s\n", cwd);
+      }
+      else
+      perror("getcwd() error");
+    }
+    /*
+    here should code scanning current directory that is
+    when -d is optional
+    */
     pDir = opendir (v[1]);
     char path[200];
     strncpy(path, v[1], strlen(v[1]));
     char* ptr = path;
+    printf("Initial PID : %d\n", (init_pid=getpid()));
+
     recur(pDir, pDirent, ptr);
 
     return 0;
